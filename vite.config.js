69d4fd9b367e49c6aa88aab01153b0741d4afe87
Prathamesh Sabale <prathameshsabale1815@gmail.com>
2026-02-02 09:44:29 +0530

    Add custom JUNO CAMPUS app icons for PWA

--- vite.config.js
+++ vite.config.js
@@ -21,10 +21,14 @@ export default defineConfig({
         orientation: 'portrait',
         icons: [
           {
-            src: 'vite.svg',
-            sizes: 'any',
-            type: 'image/svg+xml',
-            purpose: 'any maskable'
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
           }
         ]
       }
